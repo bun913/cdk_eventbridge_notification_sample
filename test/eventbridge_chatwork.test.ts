d22620b756e829625f8ddbf43caada6868696fc7
bun913 <imaizumi.taiki@classmethod.jp>
2022-12-04 14:48:27 +0900

    Initial commit

---
+++ test/eventbridge_chatwork.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EventbridgeChatwork from '../lib/eventbridge_chatwork-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/eventbridge_chatwork-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EventbridgeChatwork.EventbridgeChatworkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
