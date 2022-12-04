2adad114b808dc4356a2dabd74c4909dd5fa3e4f
bun913 <imaizumi.taiki@classmethod.jp>
2022-12-04 17:08:17 +0900

    feat: 一旦通知されるところまで確認

--- lib/eventbridge_chatwork-stack.ts
+++ lib/eventbridge_chatwork-stack.ts
@@ -1,16 +1,71 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import * as cdk from 'aws-cdk-lib';
+import { Duration, SecretValue } from 'aws-cdk-lib';
+import { Alarm, Metric, ComparisonOperator } from 'aws-cdk-lib/aws-cloudwatch';
+import {
+  ApiDestination,
+  Authorization,
+  Connection,
+  HttpParameter,
+  Rule,
+} from 'aws-cdk-lib/aws-events';
+import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class EventbridgeChatworkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'EventbridgeChatworkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // CloudWatch Alarm
+    const ec2CpuAlarm = new Alarm(this, 'testEC2CpuAlarm', {
+      metric: new Metric({
+        namespace: 'AWS/EC2',
+        metricName: 'CPUUtilization',
+        dimensionsMap: {
+          // 今回は手動で作成したEC2のIDを直で指定
+          InstanceId: 'i-',
+        },
+        statistic: 'Average',
+        period: Duration.minutes(1),
+      }),
+      evaluationPeriods: 1,
+      threshold: 0.005,
+      comparisonOperator: ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+    });
+    // ChatWorkのAPIキー
+    const secret = new Secret(this, 'Secret', {
+      secretName: 'ChatWorkApiKey',
+      generateSecretString: {
+        generateStringKey: 'password',
+        secretStringTemplate: JSON.stringify({
+          apiKey: process.env.APIKEY ?? '',
+        }),
+      },
+    });
+    //EventBridge Rule
+    const connection = new Connection(this, 'Connection', {
+      authorization: Authorization.apiKey(
+        'X-ChatWorkToken',
+        SecretValue.secretsManager(secret.secretArn, {
+          jsonField: 'apiKey',
+        })
+      ),
+      description: 'Connection with API Key X-ChatWorkToken',
+      bodyParameters: { body: HttpParameter.fromString('text, from CDK') },
+    });
+    const destination = new ApiDestination(this, 'Destination', {
+      connection,
+      endpoint: `https://api.chatwork.com/v2/rooms/${process.env.ROOMID}/messages`,
+      description: 'Calling example.com with API key x-api-key',
+    });
+    const rule = new Rule(this, 'testAlarmRule', {
+      ruleName: 'testAlarmRule',
+      eventPattern: {
+        source: ['aws.cloudwatch'],
+        detailType: ['CloudWatch Alarm State Change'],
+        resources: [ec2CpuAlarm.alarmArn],
+      },
+      targets: [new cdk.aws_events_targets.ApiDestination(destination)],
+    });
   }
 }
