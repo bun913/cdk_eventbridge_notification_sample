25bdf19ffd248f96b1993a1d96a63fd0ce149fc0
bun913 <imaizumi.taiki@classmethod.jp>
2022-12-06 12:25:02 +0900

    feat: Discord通知のサンプル完成

--- lib/eventbridge_chatwork-stack.ts
+++ lib/eventbridge_chatwork-stack.ts
@@ -14,10 +14,43 @@ import {
 } from 'aws-cdk-lib/aws-events';
 import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 import { Construct } from 'constructs';
+import {
+  AmazonLinuxCpuType,
+  AmazonLinuxGeneration,
+  AmazonLinuxImage,
+  Instance,
+  InstanceType,
+  InstanceClass,
+  InstanceSize,
+  SubnetType,
+  Vpc,
+} from 'aws-cdk-lib/aws-ec2';
 
 export class EventbridgeChatworkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    // EC2作成
+    const vpc = new Vpc(this, 'Vpc', {
+      cidr: '10.100.0.0/16',
+      maxAzs: 2,
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'isolated',
+          subnetType: SubnetType.PRIVATE_ISOLATED,
+        },
+      ],
+    });
+    const ami = new AmazonLinuxImage({
+      generation: AmazonLinuxGeneration.AMAZON_LINUX_2,
+      cpuType: AmazonLinuxCpuType.X86_64,
+    });
+    const ec2 = new Instance(this, 'sampleInstance', {
+      vpc: vpc,
+      instanceType: InstanceType.of(InstanceClass.T2, InstanceSize.MICRO),
+      machineImage: ami,
+    });
     // CloudWatch Alarm
     const ec2CpuAlarm = new Alarm(this, 'testEC2CpuAlarm', {
       metric: new Metric({
@@ -25,7 +58,7 @@ export class EventbridgeChatworkStack extends cdk.Stack {
         metricName: 'CPUUtilization',
         dimensionsMap: {
           // 今回は手動で作成したEC2のIDを直で指定
-          InstanceId: 'i-0a51f37c60f7ba142',
+          InstanceId: ec2.instanceId,
         },
         statistic: 'Average',
         period: Duration.minutes(1),
@@ -34,29 +67,38 @@ export class EventbridgeChatworkStack extends cdk.Stack {
       threshold: 0.005,
       comparisonOperator: ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
     });
-    // ChatWorkのAPIキー
-    const secret = new Secret(this, 'Secret', {
-      secretName: 'ChatWorkApiKey',
-      generateSecretString: {
-        generateStringKey: 'password',
-        secretStringTemplate: JSON.stringify({
-          apiKey: process.env.APIKEY ?? '',
-        }),
-      },
-    });
+    // const secret = new Secret(this, 'Secret', {
+    //   secretName: 'ChatWorkApiKey',
+    //   generateSecretString: {
+    //     generateStringKey: 'password',
+    //     secretStringTemplate: JSON.stringify({
+    //       apiKey: process.env.APIKEY ?? '',
+    //     }),
+    //   },
+    // });
     //EventBridge Rule
     const connection = new Connection(this, 'Connection', {
       authorization: Authorization.apiKey(
-        'X-ChatWorkToken',
-        SecretValue.secretsManager(secret.secretArn, {
-          jsonField: 'apiKey',
-        })
+        'Token if Nedded',
+        SecretValue.unsafePlainText('hoge')
+        // SecretValue.secretsManager(secret.secretArn, {
+        //   jsonField: 'apiKey',
+        // })
       ),
-      description: 'Connection with API Key X-ChatWorkToken',
+      description: 'Connection with API Key Token If Needed',
     });
+    // const connection = new Connection(this, 'Connection', {
+    //   authorization: Authorization.apiKey(
+    //     'X-ChatWorkToken',
+    //     SecretValue.secretsManager(secret.secretArn, {
+    //       jsonField: 'apiKey',
+    //     })
+    //   ),
+    //   description: 'Connection with API Key X-ChatWorkToken',
+    // });
     const destination = new ApiDestination(this, 'Destination', {
       connection,
-      endpoint: `https://api.chatwork.com/v2/rooms/${process.env.ROOMID}/messages`,
+      endpoint: process.env.ENDPOINT ?? '',
       description: 'Calling example.com with API key x-api-key',
     });
     const rule = new Rule(this, 'testAlarmRule', {
@@ -69,19 +111,11 @@ export class EventbridgeChatworkStack extends cdk.Stack {
     });
     rule.addTarget(
       new cdk.aws_events_targets.ApiDestination(destination, {
-        // ChatWork用
-        queryStringParameters: {
-          body: `$.detail`,
-          self_unread: '1',
-        },
-        // Discord用
-        // event: RuleTargetInput.fromText(
-        //   `body=Title:${EventField.fromPath('$.detail.alarmName')}`
-        // )
-        // event: RuleTargetInput.fromObject({
-        //   body: `:loudspeaker:${EventField.fromPath('$.detail.alarmName')}
-        // :new: ${EventField.fromPath('$.detail.state.reason')}`,
-        // }),
+        event: RuleTargetInput.fromObject({
+          content: `:loudspeaker: アラート :loudspeaker:
+アラート名: ${EventField.fromPath('$.detail.alarmName')}
+アラート理由: ${EventField.fromPath('$.detail.state.reason')}`,
+        }),
       })
     );
   }
